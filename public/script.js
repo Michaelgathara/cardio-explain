34fe911b80dba96eec3ae1b7b7992028459f0bb4
michaelgathara <mikegtrm@gmail.com>
2023-05-20 01:37:33 -0500

    done

--- public/script.js
+++ public/script.js
@@ -2,14 +2,17 @@ window.onload = function() {
     var wrongElements = document.getElementsByClassName('wrong');
     for (var i = 0; i < wrongElements.length; i++) {
         wrongElements[i].addEventListener('click', function() {
-            this.style.backgroundColor = 'lightred';
+            this.style.backgroundColor = 'lightcoral';
         });
     }
 
-    var rightElements = document.getElementsByClassName('right');
+    var rightElements = document.getElementsByClassName('correct');
     for (var i = 0; i < rightElements.length; i++) {
         rightElements[i].addEventListener('click', function() {
             this.style.backgroundColor = 'lightgreen';
+            // startConfetti();
+            // sleep(10);
+            // removeConfetti();
         });
     }
 }
